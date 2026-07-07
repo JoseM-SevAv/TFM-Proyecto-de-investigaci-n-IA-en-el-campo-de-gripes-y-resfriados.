a8652c8eb2231fadf492c7f0befa7f53e50187f4
José María Sevilla Avendaño <chema.lezuza@gmail.com>
2026-07-07 09:26:49 +0200

    Update ejemplo de conversión de archivo de imagen a matriz.py

--- ejemplo de conversión de archivo de imagen a matriz.py
+++ ejemplo de conversión de archivo de imagen a matriz.py
@@ -9,13 +9,44 @@ import os
 import cv2
 import numpy as np
 
-# Cargar la imagen
+# Creamos listas vacías que luego llenaremos con los datos de evaluación del modelo de Elastic Net.
 
-imagen = cv2.imread('dataset/imagenes/garganta.jpg')
+X = []
+y = []
 
-# Convertir a matriz numérica, en este caso, y a pesar de usar np.array se nos mostrará una estructura acorde a una matriz de 3 dimensiones.
+# Creamos nuestras categorías de gargantas.
 
-imagen_matrix = np.array(imagen)
+categorias = ["sano", "garganta inflamada", "garganta infectada"]
 
-# A partir de aquí, al igual que con los datos de sonido, podemos usar estos datos
-# Para un modelo de elastic net.
+# Cargamos nuestras imágenes desde la carpeta del dataset, seleccionando las tres carpetas que se corresponderan con las 3 categorías.
+
+for etiqueta, categoria in enumerate(categorias):
+
+    carpeta = os.path.join("dataset/imagenes", categoria)
+
+# Redimensionamos y escalamos las imágenes de la carpeta una por una.
+  
+    for archivo in os.listdir(carpeta):
+
+        ruta = os.path.join(carpeta, archivo)
+
+        img = cv2.imread(ruta)
+
+        # Redimensionamos.
+        img = cv2.resize(img, (64, 64))
+
+        # Escalamos valores entre 0 y 1.
+        img = img.astype(np.float32) / 255.0
+
+        # Convertimos la imagen en un vector.
+        caracteristicas = img.flatten()
+
+        # Añadimos a las listas creadas antes las características de las imágenes extraídas y sus etiquetas.
+      
+        X.append(caracteristicas)
+        y.append(etiqueta)
+
+# Finalmente pasamos nuestras listas a formato array para que puedan ser usadas en la evaluación mediante el modelo Elastic Net.
+
+X = np.array(X)
+y = np.array(y)
